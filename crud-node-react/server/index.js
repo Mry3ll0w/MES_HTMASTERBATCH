5912a73b6bd53aad485445247599974d7656cc41
MrY3ll0w <aradxc56@gmail.com>
2022-06-27 16:42:23 +0200

    con tedious funcionando

--- crud-node-react/server/index.js
+++ crud-node-react/server/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
-
-const sql = require('mysql');
-
 const app = express();
 
-//
-
+var Connection = require('tedious').Connection;  
+    var config = {  
+        server: 'localhost:1433',  //update me
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'sa', //update me
+                password: 'pasWORD1'  //update me
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: true,
+            database: 'master'  //update me
+        }
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.
+        console.log("Connectado a la base de datos");  
+    });
+    
+connection.connect();
 
 app.listen('3001', () => {
-    console.log("Servidor en 3001");
+    console.log('Aplicacion en 3001 encendida');
 });
