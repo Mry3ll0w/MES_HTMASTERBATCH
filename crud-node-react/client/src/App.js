991cd0db08522ce67171ae52b8b4578f060b85a9
MrY3ll0w <aradxc56@gmail.com>
2022-06-27 16:22:32 +0200

    antes de empezar con sql

--- crud-node-react/client/src/App.js
+++ crud-node-react/client/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 export function App() {
   return (
     <div className="App">
-      Hello world
+      <h1>Ejemplo de CRUD con nodejs y react</h1>
+      <div className = "form">
+        <label>FechaHora</label>
+        <input type = "text" name="MovieName"/>
+        <br/>
+        <label>Review</label>
+        <input type = "text" name = "Review"/>
+      </div>
     </div>
   );
 }
---
+++ crud-node-react/server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const sql = require('mysql');
+
+const app = express();
+
+//
+
+
+app.listen('3001', () => {
+    console.log("Servidor en 3001");
+});
