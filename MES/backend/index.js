ca6b2724b021821d1ffba456d5ece9b2696de6b2
MrY3ll0w <aradxc56@gmail.com>
2022-06-30 13:14:15 +0200

    Listo recibe productos

--- MES/backend/index.js
+++ MES/backend/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const sql = require('mssql')
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { json } = require('body-parser');
+const async = require('async');
 app.listen('3001',() => {console.log('listening in 3001')});
-
+//usando bodyparser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());//Permitir coger la info del front end como json
+app.use(cors());
 // DB credentials
 const config = {
     user: 'sa',
@@ -37,13 +43,13 @@ async function get_query(q) {
     try {
         const result = await DB.request()
             .query(q);
-
-        return result.recordset;
+        
+        return { query: result.recordset, ok : true};
     }
     catch (err) {
         console.log(`Error querying database, query usada ${q}`, err);
 
-        return err;
+        return {query :err, ok : false};
     }
     finally {
         DB.close();
@@ -57,4 +63,16 @@ async function execute() {
     return result;
 }
 
-execute();
+app.get('/RegEnsacado', (request, res) => {
+
+    async function query(){
+
+        let q_ensacados= await get_query("select * from MES.dbo.tbRegEnsacado");
+        let q_prods = await get_query("select Producto from MES.dbo.tbRegEnsacado GROUP by Producto");
+        if (q_ensacados.ok && q_prods.ok) res.send({Productos : q_prods.query , Ensacados : q_ensacados.query});
+        else res.send("Fallo al hacer la query");
+
+    }
+    query();
+});
+
--- MES/frontend/src/pages/Ensacado.jsx
+++ MES/frontend/src/pages/Ensacado.jsx
@@ -1,35 +1,38 @@
+
+//Visuals
+
+
+import { DataGrid } from '@mui/x-data-grid';
 import React, { Fragment } from 'react'
 import { useState } from 'react';
 import axios from 'axios'
 import { useEffect } from 'react';
 export default function RegEnsacado() {
 
-    const [Productos,setProductos] = useState([]);
-    const [ListEnsacados, SetEnsacados] = useState([]);
+    const [Productos,SetProductos] = useState([]);
+    const [Ensacados, SetEnsacados] = useState([]);
 
     //Obtenemos el resultado del get
     useEffect(() => {
         axios.get('http://172.26.0.21:3001/RegEnsacado').then((response) => {
-            setProductos(response.data.Producto);
-            SetEnsacados(response.data.ensacados);
-        })
+            SetProductos(response.data.Productos);
+            SetEnsacados(response.data.Ensacados);
+        }).catch( error => console.log(error));
     });
 
   return (
     <Fragment>
         <div>Bienvenido al registro de ensacado</div>
         <p>A continuación mostramos los ultimos ensacados</p>
-        <List>
-        {ListEnsacados.map((i) => {
-                return (
-                <Fragment>
-                    <h1>{i.Producto}</h1>
-                </Fragment>
-                )
-           })}
-        </List>
+        <div>
+
+        {Productos.map((i) =>{
+            return <h1>{i.Producto}</h1>
+        })}
+        </div>
            
         <footer> <button onClick={ event => window.location.href='/'}>Volver al menu inicial</button> </footer>
+        
     </Fragment>
     );
 }
