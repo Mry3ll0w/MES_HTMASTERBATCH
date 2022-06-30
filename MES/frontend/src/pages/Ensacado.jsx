b4f1e610b75e8ea3271e7a2bbfddc3f4a4b013ac
MrY3ll0w <aradxc56@gmail.com>
2022-06-30 16:52:23 +0200

    extrayendo seleccionado y ytal

--- MES/frontend/src/pages/Ensacado.jsx
+++ MES/frontend/src/pages/Ensacado.jsx
@@ -7,11 +7,12 @@ import React, { Fragment } from 'react'
 import { useState } from 'react';
 import axios from 'axios'
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 export default function RegEnsacado() {
-
+    
     const [Productos,SetProductos] = useState([]);
     const [Ensacados, SetEnsacados] = useState([]);
-
+    const [Seleccion, Setselected] = useState([]);
     //Obtenemos el resultado del get
     useEffect(() => {
         axios.get('http://172.26.0.21:3001/RegEnsacado').then((response) => {
@@ -20,18 +21,56 @@ export default function RegEnsacado() {
         }).catch( error => console.log(error));
     });
 
+    //Columnas 
+    const columns= [
+        {field : 'Fecha', headerName: 'Fecha', width:'150'},
+        {field : 'Turno', headerName: 'Turno', width: '90'},
+        {field : 'Producto', headerName: 'Producto', width: '150'},
+        {field : 'Palet', headerName:'Palet', width: '150'},
+        {field : 'Cantidad', headerName: 'Cantidad(KG)', width : '70'},
+        {field : 'Peso_Saco', headerName :'Peso Saco (KG)', width : '70'},
+        {field : 'Ant', headerName : 'Anterior (KG)', width : '70'}
+
+    ];
+
+    //Construimos las filas
+    let rows = [];
+    
+    Ensacados.map( (i, n) =>{
+        rows = [...rows, {id: n++, Fecha : i.Fecha, Turno : i.Turno, Producto: i.Producto, 
+            Palet: i.Palet, Cantidad : i.Cantidad, Peso_Saco : i.Peso_Saco, Ant : i.Ant}];
+    });
+    
+    
   return (
     <Fragment>
         <div>Bienvenido al registro de ensacado</div>
         <p>A continuación mostramos los ultimos ensacados</p>
-        <div>
-
-        {Productos.map((i) =>{
-            return <h1>{i.Producto}</h1>
-        })}
+        <div style={{ height: 400, width: '100%' }}>
+        
+        <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+            onSelectionModelChange={(r)=>{
+                
+                const selectedIDs = new Set(r);
+                const selectedRowData = rows.filter((row) =>
+                    selectedIDs.has(row.id)
+                );
+                console.log(selectedRowData);
+                Setselected(selectedRowData);
+            }}
+        />
         </div>
-           
-        <footer> <button onClick={ event => window.location.href='/'}>Volver al menu inicial</button> </footer>
+        <div>
+        <p>Datos seleccionados : {Seleccion.map((i => {return i.Palet}))}</p>
+        
+        
+        
+        </div>
         
     </Fragment>
     );
--- MES/frontend/src/pages/estadistico.jsx
+++ MES/frontend/src/pages/estadistico.jsx
@@ -1,15 +1,63 @@
 
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import { DataGrid } from '@mui/x-data-grid';
 import { useNavigate } from 'react-router-dom'
 //Encargado de mostrar el estadistico de determinadas tendencias en una producción realizada en un tramo horario
 export default function Estadistico() {
-    let navigate = useNavigate();
+    
+    const [Selecteds, Setselecteds] = useState([]);
+    let ids = new Set();
+    const columns = [
+      { field: 'id', headerName: 'ID', width: 70 },
+      { field: 'firstName', headerName: 'First name', width: 130 },
+      { field: 'lastName', headerName: 'Last name', width: 130 },
+      {
+        field: 'age',
+        headerName: 'Age',
+        type: 'number',
+        width: 90,
+      },
+     
+    ];
+    
+    const rows = [
+      { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+      { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+      { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+      { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+      { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+      { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+      { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+      { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+      { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    ];
   return (
     <Fragment>
-        <div>Bienvenido Selecciona a donde quieres ir </div>
-    <div>
-        <button onClick={() =>{navigate('/')}}>Volver al menu de inicio</button>
+       <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        onSelectionModelChange={ (i) =>{
+         
+          const selectedIDs = new Set(i);
+          const selectedRowData = rows.filter((row) =>
+            selectedIDs.has(row.id)
+          );
+          console.log(selectedRowData);
+          Setselecteds(selectedRowData);
+        }
+        }
+      />
     </div>
+    <p>{Selecteds.length}</p>
+    <div>{Selecteds.map((i)=>{
+      return <h1>{i.lastName}</h1>
+    })}</div>
+    
     </Fragment>
   )
 }
+
