b94ee106e224dd908054403176e8e2745c1d402b
MrY3ll0w <aradxc56@gmail.com>
2022-07-05 15:05:01 +0200

    RegEnsacado listo (modificacion)  y funcionando pasamos a mejora visual

--- MES/frontend/src/pages/Ensacado.jsx
+++ MES/frontend/src/pages/Ensacado.jsx
@@ -1,6 +1,6 @@
 
 //Visuals
-import { TextField,Button,Paper,Box, Select, MenuItem, FormControl, InputLabel, Input, makeStyles} from '@mui/material';
+import { TextField,Button,Paper,Box, Select, MenuItem, FormControl, InputLabel} from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import React, { Fragment } from 'react'
 import { useState } from 'react';
@@ -20,10 +20,20 @@ export default function RegEnsacado() {
     const [M_Resto, mresto] = useState('')
     const [M_Ant, mant] = useState('')
 
+    //Manejadores de errores
+    const [Err_palet, err_palet] = useState(false)
+    const [F_error, ferr] = useState(false);
+    const [T_error, terror] = useState(false);
+    const [Prod_error, prerror] = useState(false);
+    const [Saco_error,serror] = useState(false);
+    const [Cant_Error, cerror] = useState(false);
+    const [Resto_Error, rerror]= useState(false);
+    const [Ant_Error, aerror] = useState(false);
+
     //Constantes para trabajar con los datos 
     const [Productos,SetProductos] = useState([]);
     const [Ensacados, SetEnsacados] = useState([]);
-    const [EnsMod, SetEnsMod] = useState([]);
+    
     
     
 
@@ -40,7 +50,7 @@ export default function RegEnsacado() {
         {field : 'Fecha', headerName: 'Fecha', width:'150'},
         {field : 'Turno', headerName: 'Turno', width: '90'},
         {field : 'Producto', headerName: 'Producto', width: '150'},
-        {field : 'Palet', headerName:'Palet', width: '150'},
+        {field : 'Palet', headerName:'NºLote-NºPalet', width: '150'},
         {field : 'Cantidad', headerName: 'Cantidad(KG)', width : '120'},
         {field: 'Resto', headerName: 'Resto (KG)', width : '110'},
         {field : 'Peso_Saco', headerName :'Peso Saco (KG)', width : '130'},
@@ -52,7 +62,7 @@ export default function RegEnsacado() {
     let rows = [];
     
     Ensacados.map( (i, n) =>{
-        return rows = [...rows, {id: n++, Fecha : i.Fecha, Turno : i.Turno, Producto: i.Producto, 
+        return rows = [...rows, {id: n++, Fecha : dateFormat(i.Fecha, "yyyy-mm-dd"), Turno : i.Turno, Producto: i.Producto, 
             Palet: i.Palet, Cantidad : i.Cantidad,Resto: i.Resto, Peso_Saco : i.Peso_Saco, Ant : i.Ant}];
     });
 
@@ -60,18 +70,68 @@ export default function RegEnsacado() {
 
 
 
-
+    //Comprueba los erorres de la posible modificacion y modifica el Ensacado seleccionado
     function UpdateEnsacado(){
-        
-        axios.post('http://192.168.0.123:4001/UpdateEnsacado',
-        {
-            Fecha :M_Fecha, Turno : M_Turno, Producto : M_Producto, Palet : M_Palet, Peso_Saco : M_Peso_Saco,
-            Cantidad : M_Cantidad, Resto : M_Resto, Ant : M_Ant 
+        var ok = true;
+        //Comprobamos que se cumplan los elementos dados
+        if (M_Fecha === '' || M_Fecha.length !== 10){
+            ok = false;
+            ferr(true);
+            alert("La Fecha no puede estar en blanco, o el formato de la fecha no es el correcto")
         }
-        ).then(() => {
-            alert("Insercion realizada")
-        }).catch( err => {console.log(err)})
-        ;
+        else ferr(false);
+
+        if(M_Turno === ''){
+            ok = false;
+            terror(true);
+            alert("Selecciona un Turno de la lista inferior")
+        }
+        else terror(false);
+
+        if(M_Producto === ''){
+            ok = false;
+            prerror(true);
+            alert("Selecciona un Producto de la lista inferior");
+        }else prerror(false);
+
+        if(M_Palet === '' || M_Palet.length !== 14) {
+            ok = false;
+            err_palet(true);
+            alert("No dejes el palet en blanco o revisa el formato del mismo (xxxxxxxxxxx-xxxx) ")
+        }else err_palet(false);
+
+        if(isNaN(M_Peso_Saco) === true){
+            ok = false;
+            serror(true);
+            alert("El saco debe ser un valor numerico")
+        }else serror(false);
+
+        if(isNaN(M_Cantidad) === true){
+            ok = false;
+            cerror(true);
+            alert("La Cantidad no puede ser un valor no numerico");
+        }else cerror(false);
+
+        if(isNaN(M_Resto) === true){
+            ok = false;
+            rerror(true);
+            alert("El Resto no puede ser no numerico");
+        }else rerror(false);
+        
+        //Si todo esta correcto enviamos el post para que el backend trate la query
+        if(ok){
+            axios.post('http://192.168.0.123:4001/UpdateEnsacado',
+            {
+                Fecha :M_Fecha, Turno : M_Turno, Producto : M_Producto, Palet : M_Palet, Peso_Saco : M_Peso_Saco,
+                Cantidad : M_Cantidad, Resto : M_Resto, Ant : M_Ant 
+            }
+            ).then(() => {
+                alert("Insercion realizada")
+            }).catch( err => {console.log(err)})
+            ;
+            window.location.reload(false);
+        }
+            
     }
     
     
@@ -104,7 +164,7 @@ export default function RegEnsacado() {
                    mcant(i.Cantidad);
                    mresto(i.Resto);
                    mant(i.ant);
-                   
+                   return 0;
                 })
                 
             }}
@@ -115,7 +175,10 @@ export default function RegEnsacado() {
             <h2>Modifica el Ensacado</h2>
             
             
-            <TextField id="mdate" value={M_Fecha} onChange={e => mfecha(e.target.value)} label="Fecha" sx={{m : '3px', p:'3px'}}/>
+            <TextField 
+                id="mdate" value={M_Fecha} onChange={e => mfecha(e.target.value)} label="Fecha" sx={{m : '3px', p:'3px'}}
+                error = {F_error}
+            />
             
             <FormControl>{/* Para darle formato mas limpio a los Select*/}
                 <InputLabel>Turnos</InputLabel>
@@ -124,6 +187,8 @@ export default function RegEnsacado() {
                     defaultValue='' 
                     label='Turno'
                     onChange={e => mturno(e.target.value)}
+                    required
+                    error = {T_error}
                 >
                     <MenuItem value ={'Mañana'} >Mañana</MenuItem>
                     <MenuItem value ={'Tarde'}  >Tarde</MenuItem>
@@ -138,6 +203,7 @@ export default function RegEnsacado() {
                     defaultValue="" 
                     label='Productos'
                     onChange={e => mprod(e.target.value)}
+                    error = {Prod_error}
                 >
                     {Productos.map((i) =>{return (<MenuItem key ={i.Producto} value={i.Producto}>{i.Producto}</MenuItem>)})}
                 </Select>
@@ -146,16 +212,33 @@ export default function RegEnsacado() {
                 
             
             
-            <TextField value={M_Palet} onChange={e => mpalet(e.target.value)} label="NºLote-NºPalet" sx={{m : '3px', p:'3px'}} />
-            <TextField value={M_Peso_Saco} onChange={e => mpsaco(e.target.value)} label="Peso Saco(kg)" sx={{m : '3px', p:'3px'}}/>
+            <TextField 
+                value={M_Palet} onChange={e => mpalet(e.target.value)} label="NºLote-NºPalet" sx={{m : '3px', p:'3px'}}
+                error={Err_palet}
+            />
+            <TextField 
+                value={M_Peso_Saco} onChange={e => mpsaco(e.target.value)} label="Peso Saco(kg)" sx={{m : '3px', p:'3px'}}
+                error={Saco_error}
+            />
             <p></p>
-            <TextField value={M_Resto} onChange={e => mresto(e.target.value)} label="Resto (kg)" sx={{m : '3px', p:'3px'}} />
-            <TextField value={M_Cantidad} onChange={e => mcant(e.target.value)} label="Cantidad (kg)" sx={{m : '3px', p:'3px'}}/>
-            <TextField value={M_Ant} onChange={e => mant(e.target.value)} label="Cantidad (kg)" sx={{m : '3px', p:'3px'}}/>
+            <TextField 
+                value={M_Resto} onChange={e => mresto(e.target.value)} label="Resto (kg)" sx={{m : '3px', p:'3px'}} 
+                error= {Resto_Error}
+            />
+
+            <TextField 
+                value={M_Cantidad} onChange={e => mcant(e.target.value)} label="Cantidad (kg)" sx={{m : '3px', p:'3px'}}
+                error= {Cant_Error}
+            />
+
+            <TextField 
+                value={M_Ant} onChange={e => mant(e.target.value)} label="Anterior (kg)" sx={{m : '3px', p:'3px'}}
+                error={Ant_Error}
+            />
             
             
             </Paper>
-            <Button sx={{m : '4px'}}
+            <Button sx={{m : '10px'}}
                 onClick={UpdateEnsacado} 
                 variant="contained"
             >
