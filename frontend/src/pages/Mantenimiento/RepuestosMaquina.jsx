c072dae6b704c205e000a076e51dde78901975f3
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-11-11 15:34:20 +0100

    version viernes

--- frontend/src/pages/Mantenimiento/RepuestosMaquina.jsx
+++ frontend/src/pages/Mantenimiento/RepuestosMaquina.jsx
@@ -259,6 +259,7 @@ export default function RepuestosMaquina() {
                       SetSeleccionOpcionRepuesto("");
                       SetSeleccionOpcionCOD1("");
                       SetSeleccionOpcionCOD2("");
+                      SetaMateriales([]);
                       FetchMaquinas();
                     }}
                   >
